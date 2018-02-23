93c972f0e1d07dee9e7d81dba5df0852cb827c8c
Tyler Wickline <twick00@gmail.com>
2018-02-23 09:40:45 -0800

    Initial Commit

---
+++ HairSalon.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
